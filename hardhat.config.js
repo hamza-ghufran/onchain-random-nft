b15e80751c5e730076e8732399e8b8b76bebcf66
Hamza Ghufran <hamzazebghufran@gmail.com>
2021-12-24 13:50:24 +0530

    feat: deploy to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require('hardhat-deploy')
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,6 +15,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+require('dotenv').config()
+const MNEMOMIC = process.env.MNEMOMIC
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -21,9 +26,16 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    // rinkeby: {
-    //   accounts: {}
-    // }
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: {
+        mnemonic: MNEMOMIC
+      },
+      saveDeployments: true
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
   solidity: "0.8.0",
   namedAccounts: {
